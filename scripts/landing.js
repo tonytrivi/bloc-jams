14dccd8e28114c4fdbadb5e954b89c7e35dfd318
Tony Trivison <shambles72@hotmail.com>
2016-06-22 19:59:56 -0700

    checkpoint 23 DOM events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,7 @@
+var pointsArray = document.getElementsByClassName('point');
 
 
-var animatePoints = function () {
-    var points = document.getElementsByClassName('point');
-                
+var animatePoints = function(points){
     //console.log(points[0]);
                 
                 
@@ -18,3 +17,22 @@ var animatePoints = function () {
     };
                 
 };
+
+window.onload = function(){
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+        
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+        if( document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(pointsArray);
+        }
+        
+    });
+
+    
+}
