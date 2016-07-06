345920c744c144e895b0761ba53e60b9a3cdab01
Tony Trivison <shambles72@hotmail.com>
2016-07-05 18:34:30 -0700

    checkpoint 30 completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate = 
+var buildCollectionItemTemplate = function() {
+    var template = 
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -13,15 +14,21 @@ var collectionItemTemplate =
   + '  </div>'
   + '</div>'
   ;
-
-window.onload = function(){
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
     
-    // #3
+    return $(template);
+};
+
+
+$(window).load(function() {
+    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    
+    var $collectionContainer = $('.album-covers');
+    //remove text or other elements
+    $collectionContainer.empty();
+    
+
     for (var i = 0; i < 12; i++){
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
